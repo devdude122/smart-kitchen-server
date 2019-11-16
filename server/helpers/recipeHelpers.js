cd722a230058a38b57d064f320c6c5fa31538c76
Michael <40902786+mtrew2015@users.noreply.github.com>
2019-11-16 00:50:59 -0500

    adding files to sep repo

---
+++ server/helpers/recipeHelpers.js
@@ -0,0 +1,22 @@
+const db = require('../dbConfig');
+
+module.exports = {
+    getAllRecipes: () => {
+        return db('recipes');
+    },
+    getRecipesByUser: (author) => {
+        return db('recipes').where('author', author)
+    },
+    getRecipeById: (id) => {
+        return db('recipes').where('id', id)
+    },
+    insertRecipe:(recipe) => {
+        return db('recipes').insert(recipe)
+    },
+    updateRecipe:(id, recipe) => {
+        return db ('recipes').where('id', id).update(recipe)
+    }, 
+    deleteRecipe:(id) => {
+        return db('recipes').where('id', id).del()
+    }
+}
